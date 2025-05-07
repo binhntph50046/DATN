42355c0d13e48cbbf10252a66b71e97a3ea39c36
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-05-07 16:54:46 +0700

    update interface admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {  // nếu có middleware thì mở cái này ra 
+Route::prefix('admin')->name('admin.')->group(function () { // chưa có middleware thì dùng cái này nhé
+    Route::get('/', [DashboardController::class, 'index'] )->name('dashboard');
 });

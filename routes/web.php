5ade74fa5f11ead19d03406a05acc6bf0bdead7a
unknown <nguyenmanhcuong17042005@gmail.com>
2025-05-13 21:01:51 +0700

    pull

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\OrderController;
 use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\UserController;
+use App\Http\Controllers\Admin\VariantAttributeTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -96,6 +97,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
         'destroy' => 'users.destroy',
     ]);
 
+    // Blog
     Route::resource('blogs', BlogController::class);
     // Trang Trash
     Route::get('blogs-trash', [BlogController::class, 'trash'])

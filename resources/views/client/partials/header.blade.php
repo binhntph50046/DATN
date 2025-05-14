b3c23549f92fdf61946b88235c9a9817b22dfb2a
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-05-14 17:49:16 +0700

    update active menu

--- resources/views/client/partials/header.blade.php
+++ resources/views/client/partials/header.blade.php
@@ -11,14 +11,21 @@
 
 			<div class="collapse navbar-collapse" id="navbarsFurni">
 				<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-					<li class="nav-item active">
+					<li class="nav-item {{ Route::currentRouteName() == 'home' ? 'active' : '' }}">
 						<a class="nav-link" href="{{ route('home') }}">Home</a>
 					</li>
-					<li><a class="nav-link" href="{{ route('shop') }}">Shop</a></li>
-					<li><a class="nav-link" href="{{ route('about') }}">About us</a></li>
-					{{-- <li><a class="nav-link" href="services.html">Services</a></li> --}}
-					<li><a class="nav-link" href="{{ route('blog') }}">Blog</a></li>
-					<li><a class="nav-link" href="{{ route('contact') }}">Contact us</a></li>
+					<li class="nav-item {{ Route::currentRouteName() == 'shop' ? 'active' : '' }}">
+						<a class="nav-link" href="{{ route('shop') }}">Shop</a>
+					</li>
+					<li class="nav-item {{ Route::currentRouteName() == 'about' ? 'active' : '' }}">
+						<a class="nav-link" href="{{ route('about') }}">About us</a>
+					</li>
+					<li class="nav-item {{ Route::currentRouteName() == 'blog' ? 'active' : '' }}">
+						<a class="nav-link" href="{{ route('blog') }}">Blog</a>
+					</li>
+					<li class="nav-item {{ Route::currentRouteName() == 'contact' ? 'active' : '' }}">
+						<a class="nav-link" href="{{ route('contact') }}">Contact us</a>
+					</li>
 				</ul>
 
 				<ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
@@ -45,11 +52,11 @@
 								class="btn btn-white-outline">Explore</a></p>
 					</div>
 				</div>
-				<div class="col-lg-7">
+				{{-- <div class="col-lg-7">
 					<div class="hero-img-wrap">
 						<img src="images/couch.png" class="img-fluid">
 					</div>
-				</div>
+				</div> --}}
 			</div>
 		</div>
 	</div>
